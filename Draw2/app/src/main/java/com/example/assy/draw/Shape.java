f7c0427bada2d57be3ca4884f342ef2798f9b8f1
asioh123 <asi-o@info.cts.org.il>
2015-09-07 02:39:08 +0300

    start

---
+++ Draw2/app/src/main/java/com/example/assy/draw/Shape.java
@@ -0,0 +1,17 @@
+package com.example.assy.draw;
+
+/**
+ * Created by assy on 07/09/2015.
+ */
+public class Shape {
+
+    private int x;
+    private int y;
+    private int color;
+
+    Shape()
+
+
+
+
+}
